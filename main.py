123c5e08440f0cb46af892e3d9fcb782c8647212
macs321 <macs321@gmail.com>
2024-11-03 13:00:59 +0200

    capec

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world")
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+
+
+App = QApplication([])
+
+window = QWidget()
+
+
+text= QLabel("Hello world")
+
+line = QVBoxLayout()
+
+line.addWidget(text)
+
+window.setLayout(line)
+
+window.show()
+
+app.exec()
