198da4b663795ce1901e50a82446bb4d709636ca
macs321 <macs321@gmail.com>
2024-11-03 13:32:52 +0200

    created 0ne more file

---
+++ new.py
@@ -0,0 +1,15 @@
+print("jarik")
+import sys
+
+args = sys.argv
+print(args)
+
+second_arg = args[1]
+if second_arg == "1":
+    print("hello Dima")
+elif second_arg == "2":
+    print("hello ARtem")
+elif second_arg == "3":
+    print("hello Volodyas")
+else second_arg == "4":
+    print("hello mister ja")
